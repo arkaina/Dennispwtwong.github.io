607b7d3e866db72181adca4c6ed570d0c75625f7
Dennis Wong <arkaina@users.noreply.github.com>
2018-11-13 20:31:15 -0800

    Nov 2018 New Portfolio Redeisgn
    Overhaul of portfolio redesign to suit BEtter user flow from start to finish.

--- js/mainJS.js
+++ js/mainJS.js
@@ -2,59 +2,11 @@ $(document).ready(function(){
 
 	var windowResize = false;
 
-	if(window.matchMedia('(max-width: 40em)').matches){
+	$("a#toTop").click(function(){
+		$("html, body").animate({scrollTop:0}, "2500", "swing");
 
-		$("ul.nav-list").css("display", "none");
-
-
-	}
-
-
-	$("button.menu").click(function(){
-	 	
-
-		$("ul.nav-list").toggle();
-
-		// useMenu();
-
+		return false;
 	});
-
-	$(window).resize(function(){
-
-
-		if(window.matchMedia('(min-width: 40em)').matches){
-
-			$("ul.nav-list").css("display", "block");
-
-
-			
-			if(windowResize == false && windowResize != true){
-
-				windowResize = true;
-
-			}
-
-		}
-		else{
-
-			if(windowResize == true){
-
-				$("ul.nav-list").css("display", "none");
-
-				windowResize = false;
-
-			}
-
-		}
-
-	});
-
-	// function useMenu(){
-
-	// 	$("ul.nav-list").css("display", "block");
-
-	// 	console.log("itworked");
-	// }
 
 });
 
